70e68f9140087fedbb91db3575847f32f60759df
François Briatte <briatte@gmail.com>
2026-02-16 15:23:55 +0100

    exam formatting and instructions

--- exam-example/README.md
+++ exam-example/README.md
@@ -1,10 +1,54 @@
 # README
 
-__Data source:__ [Quality of Government Basic Dataset][qog], time-series version, January 2026. The data have been restricted to selected variables and years.
+___This is an example, ungraded exam. You do not need to submit it.___
+
+__Data source:__ [Quality of Government Basic Dataset][qog], time-series version, January 2026. The data have been restricted to selected variables and years. More information is available from the `docs` folder.
 
 [qog]: https://www.gu.se/en/quality-government/qog-data/data-downloads/standard-dataset
 
-More information is available from the `docs` folder.
+## Instructions
+
+- full student name required at the top
+- questions appear in the code sections
+- main dataset should be called `df` (as indicated in the script)
+- do _not_ modify the numbered code sections
+- do _not_ modify the folder structure
+- __all answers go into the script directly__
+
+When you are done, __submit your code__ (_after checking that it runs without errors -- see below_), __and nothing else.__ Do _not_ submit plots or the exam data, docs and folder.
+
+## Grading criteria
+
+- R code should be legible, concise, well-documented through comments, and must run without any errors (_as in class examples_)
+- __answers and comments should not go beyond 80 characters per line__ (use multiple lines if you need to write longer comments or answers)
+- the functions used in the answers can come either from base R or from any of the other loaded packages (_see top of the script_)
+
+All other student regulations apply.
+
+## Grading scheme
+
+- Questions 1--5, 7 and 9 are worth 2 points each
+- Questions 6 and 8 are worth 3 points each
+
+## Copy of the exam questions
+
+1. load the data
+2. subset to most recent year
+3. drop observations with missing values in `wdi_wip` or `ht_regtype`
+4. recode `ht_regtype` as `democratic` (`0` = `FALSE`, `1` = `TRUE`)
+5. using `wdi_gdpcapcon2015`, create `log_gpdc` (logarithmic units)
+6. correlate `wdi_wip` to `log_gdpc`
+    1. provide the correlation coefficient, rounded to 2 digits
+    2. provide a plot of the relationship
+    3. briefly comment on the relationship
+7. do democracies have more females in parliament on average?
+    1. express the difference in percentage points, rounded to 1 digit
+    2. determine whether the observed difference is statistically significant
+8. estimate linear regression models
+    1. express `wdi_wip` as a linear function of `log_gdpc`
+    2. fit the same model to democracies and non-democracies separately
+    3. compare the models and comment on their results
+9. plot the residuals of the model and interpret
 
 ## R code to generate the data extract
 
--- exam-example/exam-example.r
+++ exam-example/exam-example.r
@@ -12,53 +12,105 @@
 #
 # ==============================================================================
 
+# packages (do not modify)
+
 library(broom)
 library(texreg)
 library(tidyverse)
 
-# 1. load the data --------------------------------------------------------
+# ==============================================================================
+# 1. load the data
+# ==============================================================================
+
+df <- 
+
+# ==============================================================================
+# 2. subset to most recent year
+# ==============================================================================
 
 
 
-# 2. subset to most recent year -------------------------------------------
+# ==============================================================================
+# 3. drop observations with missing values in `wdi_wip` or `ht_regtype`
+# ==============================================================================
 
 
 
-# 3. drop observations with missing values in `wdi_wip` or `ht_regtype` ---
+# ==============================================================================
+# 4. recode `ht_regtype` as `democratic` (0 = FALSE, 1 = TRUE)
+# ==============================================================================
 
 
 
-# 4. recode `ht_regtype` as `democratic` (0 = FALSE 1 = TRUE) -------------
+# ==============================================================================
+# 5. using `wdi_gdpcapcon2015`, create `log_gpdc` (logarithmic units)
+# ==============================================================================
 
 
-
-# 5. using `wdi_gdpcapcon2015`, create `log_gpdc` (logarithmic units) -----
-
-
-
-# 6. correlate `wdi_wip` to `log_gdpc` and interpret ----------------------
+# ==============================================================================
+# 6. correlate `wdi_wip` to `log_gdpc`
+# ==============================================================================
 #
-# optionally, draw a plot of the relationship
+# 1. provide the correlation coefficient, rounded to 2 digits
+# 2. provide a plot of the relationship
+# 3. briefly comment on the relationship
 
 
 
-# 7. determine whether democracies have more females in parliament --------
+# correlation coefficient: ... 
+# ------------------------
 #
-# (1) express the difference in percentage points, rounded to 1 digit
-# (2) determine whether the observed difference is statistically significant
+# interpretation (2 lines max, 80 characters per line max)
+# --------------------------------------------------------
+# ...
+# ...
 
-
-
-# 8. estimate linear regression models ------------------------------------
+# ==============================================================================
+# 7. do democracies have more females in parliament on average?
+# ==============================================================================
 #
-# (1) express `wdi_wip` as a linear function of `log_gdpc`
-# (2) fit the same model to democracies and non-democracies separately
-# (3) compare and interpret all models
+# 1. express the difference in percentage points, rounded to 1 digit
+# 2. determine whether the observed difference is statistically significant
 
 
 
-# 9. plot the residuals of the model and interpret ------------------------
+# difference (in percentage points): ... 
+# ----------------------------------
+#
+# interpretation (3 lines max, 80 characters per line max)
+# --------------------------------------------------------
+# ...
+# ...
+# ...
+
+# ==============================================================================
+# 8. estimate linear regression models
+# ==============================================================================
+#
+# 1. express `wdi_wip` as a linear function of `log_gdpc`
+# 2. fit the same model to democracies and non-democracies separately
+# 3. compare the models and comment on their results
 
 
+
+# interpretation (5 lines max, 80 characters per line max)
+# --------------------------------------------------------
+# ...
+# ...
+# ...
+# ...
+# ...
+
+# ==============================================================================
+# 9. plot the residuals of the model and interpret
+# ==============================================================================
+
+
+
+# interpretation (3 lines max, 80 characters per line max)
+# --------------------------------------------------------
+# ...
+# ...
+# ...
 
 # thank you for your work!
